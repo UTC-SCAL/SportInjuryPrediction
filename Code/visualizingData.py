e7c40e701dc633550a0f7248e6f4a36d67ff6606
miste <misternobrows@gmail.com>
2020-03-18 12:28:05 -0400

    Redid the Y axis limits to be automated based on what variable is being graphed. Also, added in the injury and clei data for the players in the query results files.

--- Code/visualizingData.py
+++ Code/visualizingData.py
@@ -3,7 +3,7 @@ import pandas
 import numpy
 
 
-def compareGraph(preSeason, duringSeason, variable):
+def compareGraph(preSeason, duringSeason, variableGraphed, variableCut, ylimit):
     # Plot setup
     xValues = numpy.arange(len(preSeason.values))  # length of x axis
     ax1 = plt.subplot(1, 1, 1)  # subplot we'll add our data onto
@@ -11,16 +11,32 @@ def compareGraph(preSeason, duringSeason, variable):
     plt.xticks(xValues + w / 2, preSeason.candidate_id.values)  # player ID numbers
 
     # Adding the data to the graph
-    plt.ylabel("Player %s" % variable)
-    preS_values = ax1.bar(xValues, preSeason[variable].values, width=w, color='b', align='center')
-    # The trick is to use two different axes that share the same x axis, we have used ax1.twinx() method.
+    plt.ylabel("Player %s" % variableGraphed)
+    preS_values = ax1.bar(xValues, preSeason[variableGraphed].values, width=w, color='b', align='center')
+
+    # The trick is to use two different axes that share the same x axis, we have used ax1.twinx() method
+    # Since we set the y limit before calling twinx, ax2 now has the same ylimit as ax1
+    ax1.set_ylim([0, ylimit + 50])
     ax2 = ax1.twinx()
-    durS_values = ax2.bar(xValues+w, duringSeason[variable].values, width=w, color='g', align='center')
+
+    durS_values = ax2.bar(xValues+w, duringSeason[variableGraphed].values, width=w, color='g', align='center')
     plt.legend([preS_values, durS_values], ['Pre Season', 'During Season'])
+
+    # Change the title of the graph based on how you cut the data
+    plt.title("Pre Season vs During Season (No %s)" % variableCut)
     plt.show()
 
 
+variableCut = 'CLEI_PreS'  # variable used to split the data
+variableGraphed = "Avg_RT"  # variable you want to graph
+# In the cut versions of the data below, change the value after == to determine how you want the data cut
 preSeason = pandas.read_csv("../Data/Query Results Pre Season.csv")
+preSeason_cut = preSeason[preSeason[variableCut] == 0]
 duringSeason = pandas.read_csv("../Data/Query Results During Season.csv")
+duringSeason_cut = duringSeason[duringSeason[variableCut] == 0]
+
 print(preSeason.columns)
-compareGraph(preSeason, duringSeason, "correct_percent")
+# Get the maximum value possible between the two lists you want to graph
+ylimit = max(max(preSeason[variableGraphed].values), max(duringSeason[variableGraphed].values))
+
+compareGraph(preSeason_cut, duringSeason_cut, variableGraphed, variableCut, ylimit)
